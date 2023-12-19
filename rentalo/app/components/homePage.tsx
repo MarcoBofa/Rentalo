3e2b6b28b78fa947682675a1f4e4665e82066abd
Marco Bonafini <marcobonafini@Marcos-MacBook-Pro.local>
2023-12-19 02:31:04 -0600

    design

--- rentalo/app/components/homePage.tsx
+++ rentalo/app/components/homePage.tsx
@@ -8,7 +8,7 @@ import InfoForm from "./homepage/infoForm";
 const homePage: FC = () => {
   return (
     <div className="flex flex-col">
-      <div className="">
+      <div className="relative">
         <Image
           src="/cantiere-stradale.jpg"
           layout="responsive"
@@ -17,17 +17,17 @@ const homePage: FC = () => {
           alt="Home"
         />
         <div className="absolute top-0 left-0 w-full h-full bg-gray-900 opacity-90"></div>
-        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center">
-          <h1 className="text-white text-lg md:text-7xl titlehome font-bold mb-5">
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center p-4">
+          <h1 className="text-white text-3xl md:text-7xl titlehome font-bold mb-5 text-center">
             BENVENUTO SU RENTALO
           </h1>
-          <h1 className="text-white text-1xl font-bold mb-2">
+          <h2 className="text-white text-lg md:text-1xl font-bold mb-2 text-center">
             Trova tutti i mecchinari di cui hai bisogno e gestisci i tuoi
             annunci comodamente dalla piattaforma
-          </h1>
-          <div className="flex space-x-4">
+          </h2>
+          <div className="mt-10">
             <Link href="/ScegliUtente">
-              <button className="text-black text-1xl titlehome bg-orange-500 py-2 px-4 w-[250px] h-[55px] homebtn mt-10">
+              <button className="text-black bg-orange-500 py-2 px-4 w-full md:w-[250px] h-[55px] homebtn">
                 Registrati
               </button>
             </Link>
@@ -35,9 +35,10 @@ const homePage: FC = () => {
         </div>
       </div>
 
-      <div className="bg-white w-full">
+      <div className="bg-white w-full p-4">
         <InfoForm />
       </div>
+
       <div className="relative">
         <Image
           src="/Cantiere-mezzi-lavoro.jpeg"
