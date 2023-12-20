89dafdfce0f5381326091c4256367ecb4f2a70f0
Marco Bonafini <marcobonafini@Marcos-MacBook-Pro.local>
2023-12-19 21:14:26 -0600

    design

--- rentalo/pages/RegistraNoleggiatore.tsx
+++ rentalo/pages/RegistraNoleggiatore.tsx
@@ -199,85 +199,80 @@ const RegistraNoleggiatore: React.FC = () => {
           </form>
         </div>
       </div>
-      <div className="hidden">
-        <div className="w-1/2 p-10 flex flex-col items-center justify-center relative bg-black">
-          <div className="absolute inset-0">
-            <svg
-              className="w-full h-full"
-              viewBox="0 0 100 100"
-              preserveAspectRatio="none"
-            >
-              <defs>
-                <linearGradient
-                  id="greyGradient"
-                  x1="0%"
-                  y1="100%"
-                  x2="100%"
-                  y2="0%"
-                >
-                  <stop
-                    offset="0%"
-                    style={{ stopColor: "rgba(130, 62, 5, 1)" }}
-                  />
-                  <stop
-                    offset="60%"
-                    style={{ stopColor: "rgba(0, 0, 0, 1)" }}
-                  />
-                </linearGradient>
-                <clipPath id="triangleClip">
-                  <polygon points="0,100 100,0 100,100" />
-                </clipPath>
-              </defs>
-              <rect
-                x="0"
-                y="0"
-                width="100"
-                height="100"
-                fill="url(#greyGradient)"
-                clipPath="url(#triangleClip)"
-              />
-            </svg>
-          </div>
+      <div className="w-1/2 p-10 flex flex-col items-center justify-center relative bg-black">
+        <div className="absolute inset-0">
+          <svg
+            className="w-full h-full"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+          >
+            <defs>
+              <linearGradient
+                id="greyGradient"
+                x1="0%"
+                y1="100%"
+                x2="100%"
+                y2="0%"
+              >
+                <stop
+                  offset="0%"
+                  style={{ stopColor: "rgba(130, 62, 5, 1)" }}
+                />
+                <stop offset="60%" style={{ stopColor: "rgba(0, 0, 0, 1)" }} />
+              </linearGradient>
+              <clipPath id="triangleClip">
+                <polygon points="0,100 100,0 100,100" />
+              </clipPath>
+            </defs>
+            <rect
+              x="0"
+              y="0"
+              width="100"
+              height="100"
+              fill="url(#greyGradient)"
+              clipPath="url(#triangleClip)"
+            />
+          </svg>
+        </div>
 
-          <div className="text-center z-10 mt-20 flex flex-col justify-center">
-            <div className="mb-40 flex items-start justify-center">
-              <span className="text-orange-500 border border-orange-500 rounded-full w-8 h-8 flex items-center justify-center mr-4 mt-1">
-                <i className="fas fa-check"></i>
-              </span>
-              <div className="text-center">
-                <h2 className="text-3xl text-orange-500 font-bold mb-3">
-                  Crea il tuo account in pochi minuti!
-                </h2>
-                <p className="text-md text-white">
-                  registrazione facile e veloce
-                </p>
-              </div>
+        <div className="text-center z-10 mt-20 flex flex-col justify-center">
+          <div className="mb-40 flex items-start justify-center">
+            <span className="text-orange-500 border border-orange-500 rounded-full w-8 h-8 flex items-center justify-center mr-4 mt-1">
+              <i className="fas fa-check"></i>
+            </span>
+            <div className="text-center">
+              <h2 className="text-3xl text-orange-500 font-bold mb-3">
+                Crea il tuo account in pochi minuti!
+              </h2>
+              <p className="text-md text-white">
+                registrazione facile e veloce
+              </p>
             </div>
-            <div className="mb-40 flex items-start justify-center">
-              <span className="text-orange-500 border border-orange-500 rounded-full w-8 h-8 flex items-center justify-center mr-4 mt-1">
-                <i className="fas fa-check"></i>
-              </span>
-              <div className="text-center">
-                <h2 className="text-3xl text-orange-500 font-bold mb-3">
-                  Noleggia quello che ti serve
-                </h2>
-                <p className="text-md text-white">
-                  Trova tutti i macchinari e le attrezzature di cui hai bisogno
-                </p>
-              </div>
+          </div>
+          <div className="mb-40 flex items-start justify-center">
+            <span className="text-orange-500 border border-orange-500 rounded-full w-8 h-8 flex items-center justify-center mr-4 mt-1">
+              <i className="fas fa-check"></i>
+            </span>
+            <div className="text-center">
+              <h2 className="text-3xl text-orange-500 font-bold mb-3">
+                Noleggia quello che ti serve
+              </h2>
+              <p className="text-md text-white">
+                Trova tutti i macchinari e le attrezzature di cui hai bisogno
+              </p>
             </div>
-            <div className="mb-40 flex items-start justify-center">
-              <span className="text-orange-500 border border-orange-500 rounded-full w-8 h-8 flex items-center justify-center mr-4 mt-1">
-                <i className="fas fa-check"></i>
-              </span>
-              <div className="text-center">
-                <h2 className="text-3xl text-orange-500 font-bold mb-3">
-                  Gestione facile e pratica
-                </h2>
-                <p className="text-md text-white">
-                  Gestisci comodamente noleggi e annunci dalla piattaforma
-                </p>
-              </div>
+          </div>
+          <div className="mb-40 flex items-start justify-center">
+            <span className="text-orange-500 border border-orange-500 rounded-full w-8 h-8 flex items-center justify-center mr-4 mt-1">
+              <i className="fas fa-check"></i>
+            </span>
+            <div className="text-center">
+              <h2 className="text-3xl text-orange-500 font-bold mb-3">
+                Gestione facile e pratica
+              </h2>
+              <p className="text-md text-white">
+                Gestisci comodamente noleggi e annunci dalla piattaforma
+              </p>
             </div>
           </div>
         </div>
