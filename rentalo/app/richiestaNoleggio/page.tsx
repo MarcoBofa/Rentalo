9a7a197d33adcb1287f53678c634a5e4ec97ab59
Marco Bonafini <marcobonafini@Marcos-MacBook-Pro.local>
2023-12-20 15:17:22 -0600

    noleggio form

--- rentalo/app/richiestaNoleggio/page.tsx
+++ rentalo/app/richiestaNoleggio/page.tsx
@@ -1,19 +1,22 @@
 "use client";
 import React, { FC, useState } from "react";
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 import "@/app/globals.css";
 import ToasterProvider from "@/providers/ToasterProvider";
 import toast from "react-hot-toast";
 import axios from "axios";
 
+interface macchinario {
+  regione: string;
+  descrizione: string;
+  tipo: string;
+}
+
 interface InfoFormData {
   nome: string;
   telefono: string;
   email: string;
-  regione: string;
-  descrizione: string;
-  noleggio: string;
-  tipo: string;
+  macchinario: macchinario[];
 }
 
 type FocusedState = {
@@ -25,13 +28,11 @@ const InfoForm: FC = () => {
     nome: false,
     telefono: false,
     email: false,
-    regione: false,
-    descrizione: false,
-    tipo: false,
   });
 
   const {
     register,
+    control,
     handleSubmit,
     reset,
     watch,
@@ -41,10 +42,13 @@ const InfoForm: FC = () => {
       nome: "",
       telefono: "",
       email: "",
-      regione: "",
-      descrizione: "",
-      tipo: "",
+      macchinario: [{ regione: "", tipo: "", descrizione: "" }],
     },
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "macchinario",
   });
 
   // Watch all form fields
@@ -59,7 +63,7 @@ const InfoForm: FC = () => {
   };
 
   const onSubmit = (data: InfoFormData) => {
-    console.log("Form data:", data);
+    //console.log("Form data:", data);
 
     // axios
     //   .get(
@@ -81,15 +85,17 @@ const InfoForm: FC = () => {
     if (Object.keys(errors).length > 0) {
       console.log("Errors:", errors);
     }
+    let lenght = data.macchinario.length - 1;
     if (
       data.nome == "" ||
       data.telefono == "" ||
       data.email == "" ||
-      data.regione == "" ||
-      data.descrizione == "" ||
-      data.tipo == ""
+      data.macchinario[lenght].regione == "" ||
+      data.macchinario[lenght].tipo == "" ||
+      data.macchinario[lenght].descrizione == ""
     ) {
       toast.error("Riempire tutti i campi per mandare la richiesta");
+      return;
     }
 
     axios
@@ -116,7 +122,7 @@ const InfoForm: FC = () => {
   };
 
   return (
-    <div className="flex flex-col form-background items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex flex-col form-background items-center justify-center min-h-screen bg-gray-100 pt-[120px]">
       <ToasterProvider />
       <div className="w-full max-w-2xl">
         <h2 className="text-2xl md:text-3xl font-bold text-gray-900 mb-8 text-center">
@@ -179,98 +185,126 @@ const InfoForm: FC = () => {
               onBlur={() => handleBlur("email")}
             />
           </div>
-          <div className="mb-4 flex flex-row ">
-            <div className="w-1/2 mr-5">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="regione"
-              >
-                Regione
-              </label>
-              <select
-                id="regione"
-                className={`shadow border ${
-                  focused["regione"] || watchedFields["regione"]
-                    ? "border-orange-500"
-                    : "border-gray-300"
-                } rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
-                {...register("regione")} // Register the select element
-                onFocus={() => handleFocus("regione")}
-                onBlur={() => handleBlur("regione")}
-              >
-                <option value="">Seleziona una regione</option>
-                <option value="Abruzzo">Abruzzo</option>
-                <option value="Basilicata">Basilicata</option>
-                <option value="Calabria">Calabria</option>
-                <option value="Campania">Campania</option>
-                <option value="Emilia-Romagna">Emilia-Romagna</option>
-                <option value="Friuli-Venezia Giulia">
-                  Friuli-Venezia Giulia
-                </option>
-                <option value="Lazio">Lazio</option>
-                <option value="Liguria">Liguria</option>
-                <option value="Lombardia">Lombardia</option>
-                <option value="Marche">Marche</option>
-                <option value="Molise">Molise</option>
-                <option value="Piemonte">Piemonte</option>
-                <option value="Puglia">Puglia</option>
-                <option value="Sardegna">Sardegna</option>
-                <option value="Sicilia">Sicilia</option>
-                <option value="Toscana">Toscana</option>
-                <option value="Trentino-Alto Adige">Trentino-Alto Adige</option>
-                <option value="Umbria">Umbria</option>
-                <option value="Valle d'Aosta">Valle d'Aosta</option>
-                <option value="Veneto">Veneto</option>
-              </select>
-            </div>
-            <div className="w-1/2 ml-2">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="tipo"
-              >
-                Tipo di macchinario
-              </label>
-              <select
-                id="tipo"
-                className={`shadow border ${
-                  focused["tipo"] || watchedFields["tipo"]
-                    ? "border-orange-500"
-                    : "border-gray-300"
-                } rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
-                {...register("tipo")}
-                onFocus={() => handleFocus("tipo")}
-                onBlur={() => handleBlur("tipo")}
-              >
-                <option value="">Seleziona un tipo</option>
-                <option value="grande">Grandi Macchinari</option>
-                <option value="medi">Medi Macchinari</option>
-                <option value="piccoli">Piccoli Macchinari</option>
-              </select>
-            </div>
-          </div>
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="messaggio"
+
+          {fields.map((item, index) => (
+            <div
+              key={item.id}
+              className={`mb-4 ${
+                index !== 0 ? "pt-4 border-t-2 border-gray-300" : ""
+              }`}
             >
-              Macchinario
-            </label>
-            <textarea
-              className={inputStyle("descrizione")}
-              id="descrizione"
-              placeholder="Descrivi brevemente il macchinario"
-              rows={5}
-              {...register("descrizione")}
-              onFocus={() => handleFocus("descrizione")}
-              onBlur={() => handleBlur("descrizione")}
-            />
-          </div>
-          <div className="flex items-center justify-center justify-between">
+              <div className="mb-4 flex flex-row ">
+                <div className="w-1/2 mr-5">
+                  <label
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                    htmlFor="regione"
+                  >
+                    Regione
+                  </label>
+                  <select
+                    className={`shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
+                    {...register(`macchinario.${index}.tipo`)}
+                    {...register(`macchinario.${index}.regione`)}
+                    onFocus={() => handleFocus(`macchinario.${index}.regione`)}
+                    onBlur={() => handleBlur(`macchinario.${index}.regione`)}
+                  >
+                    <option value="">Seleziona una regione</option>
+                    <option value="Abruzzo">Abruzzo</option>
+                    <option value="Basilicata">Basilicata</option>
+                    <option value="Calabria">Calabria</option>
+                    <option value="Campania">Campania</option>
+                    <option value="Emilia-Romagna">Emilia-Romagna</option>
+                    <option value="Friuli-Venezia Giulia">
+                      Friuli-Venezia Giulia
+                    </option>
+                    <option value="Lazio">Lazio</option>
+                    <option value="Liguria">Liguria</option>
+                    <option value="Lombardia">Lombardia</option>
+                    <option value="Marche">Marche</option>
+                    <option value="Molise">Molise</option>
+                    <option value="Piemonte">Piemonte</option>
+                    <option value="Puglia">Puglia</option>
+                    <option value="Sardegna">Sardegna</option>
+                    <option value="Sicilia">Sicilia</option>
+                    <option value="Toscana">Toscana</option>
+                    <option value="Trentino-Alto Adige">
+                      Trentino-Alto Adige
+                    </option>
+                    <option value="Umbria">Umbria</option>
+                    <option value="Valle d'Aosta">Valle d'Aosta</option>
+                    <option value="Veneto">Veneto</option>
+                  </select>
+                </div>
+                <div className="w-1/2 ml-2">
+                  <label
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                    htmlFor="tipo"
+                  >
+                    Tipo di macchinario
+                  </label>
+                  <select
+                    className={`shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
+                    {...register(`macchinario.${index}.tipo`)}
+                    onFocus={() => handleFocus(`macchinario.${index}.tipo`)}
+                    onBlur={() => handleBlur(`macchinario.${index}.tipo`)}
+                  >
+                    <option value="">Seleziona un tipo</option>
+                    <option value="grande">Grandi Macchinari</option>
+                    <option value="medi">Medi Macchinari</option>
+                    <option value="piccoli">Piccoli Macchinari</option>
+                  </select>
+                </div>
+              </div>
+              <div className="mb-6">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="messaggio"
+                >
+                  Macchinario
+                </label>
+                <textarea
+                  className={`shadow appearance-none border "border-orange-500" : "border-gray-300"
+                } rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
+                  placeholder="Descrivi brevemente il macchinario"
+                  rows={5}
+                  {...register(`macchinario.${index}.descrizione`)}
+                  onFocus={() =>
+                    handleFocus(`macchinario.${index}.descrizione`)
+                  }
+                  onBlur={() => handleBlur(`macchinario.${index}.descrizione`)}
+                />
+                {fields.length > 1 && (
+                  <button
+                    type="button"
+                    className="mt-2 bg-red-500 text-white rounded-sm removebtn"
+                    onClick={() => remove(index)}
+                  >
+                    Rimuovi
+                  </button>
+                )}
+              </div>
+            </div>
+          ))}
+
+          <div className="flex items-center justify-between mt-4">
+            {/* Placeholder div to balance the layout */}
+            <div></div>
+
+            {/* Invia Button - Centered */}
             <button
-              className="bg-orange-500 hover:bg-orange-700 text-white w-[200px] text-lg font-bold py-3 px-6 rounded focus:outline-none focus:shadow-outline mx-auto block"
+              className="bg-orange-500 hover:bg-orange-700 text-white w-[200px] text-lg font-bold ml-[120px] py-3 px-6 rounded focus:outline-none focus:shadow-outline"
               type="submit"
             >
               Invia
+            </button>
+
+            {/* Add Macchinario Button - Right */}
+            <button
+              type="button"
+              className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
+              onClick={() => append({ regione: "", tipo: "", descrizione: "" })}
+            >
+              Aggiungi Macchinario
             </button>
           </div>
         </form>
