ff47dba89bb3fa574f6e176a1db3fa74129644bd
Marco Bonafini <marcobonafini@Marcos-MacBook-Pro.local>
2023-12-20 15:17:22 -0600

    noleggio form

--- rentalo/app/api/richiestaMac/route.ts
+++ rentalo/app/api/richiestaMac/route.ts
@@ -3,34 +3,39 @@ import prisma from "@/app/libs/prismadb";
 
 export async function POST(request: Request) {
   const body = await request.json();
-  const { nome, telefono, email, regione, descrizione, tipo } = body;
+  const { nome, telefono, email, macchinario } = body; // Note that this should be 'macchinari' to match the model
 
   if (
     nome == null ||
     telefono == null ||
     email == null ||
-    regione == null ||
-    descrizione == null ||
-    tipo == null
+    macchinario[0].regione == "" ||
+    macchinario[0].tipo == "" ||
+    macchinario[0].descrizione == ""
   ) {
     return NextResponse.json({ error: "Missing fields" }, { status: 400 });
   }
 
-  // let n = true;
-  // if (noleggio === "noleggio") {
-  //   n = true;
-  // } else {
-  //   n = false;
-  // }
-
-  const m = await prisma.macchinario.create({
+  // Create the richiestaNoleggio and associated macchinari
+  const m = await prisma.richiestaNoleggio.create({
     data: {
       nome,
       telefono,
       email,
-      regione,
-      tipo,
-      descrizione,
+      // Create the related Macchinario records
+      macchinari: {
+        create: macchinario.map(
+          (macchinario: {
+            tipo: string;
+            regione: string;
+            descrizione: string;
+          }) => ({
+            tipo: macchinario.tipo,
+            regione: macchinario.regione,
+            descrizione: macchinario.descrizione,
+          })
+        ),
+      },
     },
   });
 
