8a42aaf9496800a50c6bc19698db3b1abd2c3809
MarcoBofa <marco.bonafini@mail.polimi.it>
2023-12-26 16:48:42 -0600

    responsiveness

--- rentalo/app/Impostazioni/page.tsx
+++ rentalo/app/Impostazioni/page.tsx
@@ -5,20 +5,20 @@ import Link from "next/link";
 const DefaultImpostazioniPage: React.FC = () => {
   return (
     <ImpostazioniLayout>
-      <div className="flex flex-col items-center min-h-screen text-textSettings bg-settings py-2 py-2 px-1 lg:px-0">
-        <div className="flex flex-col items-center justify-center mt-[50px] lg:mt-[200px] w-3/4 lg:w-1/2 rounded h-[350px]p-[10px] lg:p-[40px] bg-white">
+      <div className="flex flex-col items-center min-h-screen text-textSettings bg-settings py-2 px-4 lg:px-0">
+        <div className="flex flex-col items-center justify-center mt-[50px] lg:mt-[200px]  w-full lg:w-1/ rounded h-[350px] p-[10px] lg:p-[40px] bg-white">
           <h1 className="text-xl font-bold mb-2">Impostazioni</h1>
           <h2 className="text-md mb-10 ">
             Modifica le informazioni del tuo profilo
           </h2>
           <div className="flex flex-row mb-4">
-            <Link href="/Impostazioni/password" className="mr-10">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <Link href="/Impostazioni/password" className="mr-5 lg:mr-10">
+              <button className="bg-blue-500 hover:bg-blue-700 text-sm lg:text-base text-white font-bold py-2 px-4 rounded">
                 Password
               </button>
             </Link>
             <Link href="/Impostazioni/credenziali">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              <button className="bg-blue-500 hover:bg-blue-700 text-sm lg:text-base text-white font-bold py-2 px-4 rounded">
                 Credenziali
               </button>
             </Link>
