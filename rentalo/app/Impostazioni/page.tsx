14407b74abd7051be4c1ea528c2a45aea8f22279
MarcoBofa <marco.bonafini@mail.polimi.it>
2023-12-26 16:50:53 -0600

    responsiveness

--- rentalo/app/Impostazioni/page.tsx
+++ rentalo/app/Impostazioni/page.tsx
@@ -6,7 +6,7 @@ const DefaultImpostazioniPage: React.FC = () => {
   return (
     <ImpostazioniLayout>
       <div className="flex flex-col items-center min-h-screen text-textSettings bg-settings py-2 px-4 lg:px-0">
-        <div className="flex flex-col items-center justify-center mt-[50px] lg:mt-[200px]  w-full lg:w-1/ rounded h-[350px] p-[10px] lg:p-[40px] bg-white">
+        <div className="flex flex-col items-center justify-center mt-[50px] lg:mt-[200px]  w-full lg:w-1/2 rounded h-[350px] p-[10px] lg:p-[40px] bg-white">
           <h1 className="text-xl font-bold mb-2">Impostazioni</h1>
           <h2 className="text-md mb-10 ">
             Modifica le informazioni del tuo profilo
