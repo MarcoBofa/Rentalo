1e2a027f644f84cfca82a0d1561ef2c6dd016e6c
MarcoBofa <marco.bonafini@mail.polimi.it>
2024-05-13 11:39:42 -0500

    infobox

--- rentalo/app/dashboard/parco-macchine/aggiungi/page.tsx
+++ rentalo/app/dashboard/parco-macchine/aggiungi/page.tsx
@@ -287,7 +287,7 @@ const aggiungiMacchinario: React.FC = () => {
                   <div className="flex hoverable-button justify-center items-center w-[13px] h-[13px] ml-1 mb-2 rounded-full bg-gray-200 text-xs leading-none cursor-pointer">
                     i
                   </div>
-                  <span className="display-on-hover absolute top-[-85px] left-[0px] lg:top-0 lg:left-[150px] z-10 left-full ml-2 w-[80px] lg:w-[220px] p-2 text-white mt-[-10px] bg-gray-400 text-xs rounded-lg shadow">
+                  <span className="display-on-hover absolute top-[-85px] left-[-150px] lg:top-0 lg:left-[150px] z-20 left-full ml-2 w-[80px] lg:w-[220px] p-2 text-white mt-[-10px] bg-gray-400 text-xs rounded-lg shadow">
                     Inserisci il peso del macchinario in{" "}
                     <span className="font-bold underline">kg.</span>
                   </span>
