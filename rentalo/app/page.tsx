7279dc5543093cac75b424fffdc79b853f593570
MarcoBofa <marco.bonafini@mail.polimi.it>
2024-07-27 20:19:53 -0500

    test

--- rentalo/app/page.tsx
+++ rentalo/app/page.tsx
@@ -2,7 +2,7 @@ import HomePage from "./components/homepage/homePage";
 
 export default function Home() {
   return (
-    <div className="flex flex-col pt-[84px]">
+    <div className="flex flex-col pt-[85px]">
       <HomePage />
     </div>
   );
