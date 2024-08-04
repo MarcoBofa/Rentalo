cc1e829221c1beaa49e358ad0e660eac9575fc8b
MarcoBofa <marco.bonafini@mail.polimi.it>
2024-08-03 23:58:52 -0500

    annunci page

--- rentalo/app/dashboard/annunci/page.tsx
+++ rentalo/app/dashboard/annunci/page.tsx
@@ -21,15 +21,15 @@ const ParcoMacchine: React.FC = () => {
       <div className="flex flex-col items-center h-full text-textSettings bg-settings py-2 px-4 lg:px-0">
         <div className="flex flex-col items-center justify-center mt-[155px] lg:mt-[200px] w-full lg:w-1/2 rounded h-[350px] p-[10px] lg:p-[40px] bg-white">
           <h1 className="text-xl font-bold mb-2">Dashboard</h1>
-          <h2 className="text-md mb-10 ">Gestisci il tuo parco macchine</h2>
+          <h2 className="text-md mb-10 ">Gestisci i tuoi annunci</h2>
           <div className="flex flex-row mb-4">
             <Link href="/dashboard/annunci/crea" className="mr-5 lg:mr-10">
-              <button className="bg-blue-500 hover:bg-blue-700 text-sm lg:text-base text-white font-bold py-2 px-4 rounded">
-                Aggiungi
+              <button className="bg-blue-500 w-[140px] hover:bg-blue-700 text-sm lg:text-base text-white font-bold py-2 px-4 rounded">
+                Crea annuncio
               </button>
             </Link>
             <Link href="/dashboard/annunci/gestisci">
-              <button className="bg-blue-500 hover:bg-blue-700 text-sm lg:text-base text-white font-bold py-2 px-4 rounded">
+              <button className="bg-blue-500 w-[140px] hover:bg-blue-700 text-sm lg:text-base text-white font-bold py-2 px-4 rounded">
                 Gestisci
               </button>
             </Link>
